2ed50e15e6ba4d715180d41ad771d1deae21cb77
Rohit Jaiswal <rohit.jaiswal@gojek.com>
2023-09-22 16:14:40 +0530

    adding store layer for createOTP

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -11,3 +11,12 @@ func NewHandler(service service.OTPService) *Handler {
 		service: service,
 	}
 }
+
+/**
+createOTP system
+
+createOTP API -> generate a OTP -> map to the userID provided -> insert into a otp table with createdAt timestamp
+validateOTP API -> parse the req -> userID -> fetch the otp db -> if otp exists -> createdat + 2min < current time of the request
+
+cache -> redis -> expiry for 120 seconds
+*/
