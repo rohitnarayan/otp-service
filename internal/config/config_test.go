e6cec6c556cf379cf640d2e5d134dbe1bbb018d3
Rohit Jaiswal <rohit.jaiswal@gojek.com>
2023-09-23 17:57:58 +0530

    Fix unit test

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -10,14 +10,14 @@ import (
 func TestConfig(t *testing.T) {
 	InitTestConfig()
 
-	assert.Equal(t, App.Server.Name, "leaderboard")
+	assert.Equal(t, App.Server.Name, "otp-service")
 	assert.Equal(t, App.Server.Port, 8085)
-	assert.Equal(t, App.Database.Postgres.DatabaseName, "leaderboard_production")
+	assert.Equal(t, App.Database.Postgres.DatabaseName, "otp_service")
 	assert.Equal(t, App.Database.Postgres.Host, "localhost")
 	assert.Equal(t, App.Database.Postgres.Port, 5432)
 	assert.Equal(t, App.Database.Postgres.Driver, "postgres")
 	assert.Equal(t, App.Database.Postgres.WriteTimeout, time.Millisecond*time.Duration(200))
 	assert.Equal(t, App.Database.Postgres.ReadTimeout, time.Millisecond*time.Duration(50))
-	assert.Equal(t, App.Database.Postgres.Username, "leaderboard")
+	assert.Equal(t, App.Database.Postgres.Username, "postgres")
 	assert.Equal(t, App.Database.Postgres.MaxOpenConnections, 10)
 }
--- internal/postgres/postgres_test.go
+++ internal/postgres/postgres_test.go
@@ -27,15 +27,15 @@ func TestDBMethods(t *testing.T) {
 			Args:  []interface{}{1},
 		},
 		{
-			Query: "INSERT INTO otp VALUES($1, $2, $3,$4)",
-			Args:  []interface{}{4534, "User1", time.Now().UTC(), "created"},
+			Query: "INSERT INTO otp(otp,user_id,created_at,updated_at,status) VALUES($1, $2, $3,$4,$5)",
+			Args:  []interface{}{4534, "User1", time.Now().UTC(), time.Now().UTC(), "created"},
 		},
 		{
 			Query: "SELECT * FROM otp WHERE otp=$1",
 			Args:  []interface{}{4534},
 		},
 		{
-			Query: "DELETE FROM employee WHERE otp=$1",
+			Query: "DELETE FROM otp WHERE otp=$1",
 			Args:  []interface{}{4534},
 		},
 	}
@@ -44,11 +44,8 @@ func TestDBMethods(t *testing.T) {
 	db, _ := NewDB(config.App.Database.Postgres)
 
 	var otps []*otpStore
-	err := Get(ctx, db, config.App.Database.Postgres.ReadTimeout, &otps, query[0])
-	assert.Equal(t, err, nil)
-	assert.Equal(t, 4534, otps[0].Otp)
 
-	err = Exec(ctx, db, config.App.Database.Postgres.WriteTimeout, query[3])
+	err := Exec(ctx, db, config.App.Database.Postgres.WriteTimeout, query[3])
 	assert.Equal(t, nil, err)
 
 	err = Exec(ctx, db, config.App.Database.Postgres.WriteTimeout, query[1])
